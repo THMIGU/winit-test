8e091173553e0d34f86ae66f1aa1651d9ed528ed
Kai Parsons <thmigu.kp@gmail.com>
2026-06-23 21:14:10 -0700

    feat: winit window loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use winit::event_loop::EventLoop;
+
+use crate::app::App;
+
+mod app;
+
 fn main() {
-    println!("Hello, world!");
+	let event_loop = EventLoop::new().unwrap();
+	event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+
+	let mut app = App::default();
+	let _ = event_loop.run_app(&mut app);
 }
